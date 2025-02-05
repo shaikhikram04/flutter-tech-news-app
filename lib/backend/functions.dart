4b7ff583b085a964a79ed56a2abc06a837a8f536
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-02-05 14:35:12 +0530

    created searchbar

--- lib/backend/functions.dart
+++ lib/backend/functions.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:tech_news/components/search_bar.dart';
 
 Future<List> fetchNews() async {
   final key = dotenv.get('NEWS_API_KEY');
   final response = await http.get(
     Uri.parse(
-      'https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=100&apiKey=$key&q=${SearchBar.searchcontroller.text}',
+      'https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=100&apiKey=$key&q=${SearchBar.searchController.text}',
     ),
   );
 
---
+++ lib/components/search_bar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tech_news/backend/functions.dart';
+import 'package:tech_news/utils/colors.dart';
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({super.key});
+
+  static final searchController = TextEditingController(text: '');
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            height: 50,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.only(left: 20),
+            decoration: BoxDecoration(
+              color: AppColor.darkGrey,
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      controller: SearchBar.searchController,
+                      decoration: InputDecoration(
+                        hintText: 'Search a Keyword or a Phrase',
+                        hintStyle: GoogleFonts.lato(),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+            fetchNews();
+          },
+          child: Container(
+            width: 45,
+            height: 45,
+            decoration: BoxDecoration(
+              color: AppColor.darkGrey,
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              Icons.search,
+              color: AppColor.white,
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+}
