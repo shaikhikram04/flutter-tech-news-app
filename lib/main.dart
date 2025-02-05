9afe141ee8f40bd6780082abd3b91cfbebeb229c
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-02-05 14:17:15 +0530

    Define a theme of application

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tech_news/utils/colors.dart';
 
 void main() {
+  dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tech News App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: AppColor.primary,
+      ),
     );
   }
 }
