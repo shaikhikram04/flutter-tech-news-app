73912731b631c0a37d8a4df9bdb111aa2b860f69
Ikram Shaikh <ikramshaikh0480@gmail.com>
2025-02-19 11:29:19 +0530

    completed with tech newz

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tech_news/backend/functions.dart';
 import 'package:tech_news/utils/colors.dart';
 
 class SearchBar extends StatelessWidget {
